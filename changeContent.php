ac1d2207be18cc1e9f1b7c0cd3aec3ab5332f5b5
brechoire <brechoire.j@gmail.com>
2016-06-29 20:51:09 +0200

    Ajout de la page changeContent et ajout du lien

---
+++ changeContent.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'; ?>
+
+<?php include 'footerHeader.php'; ?>
+
+<?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -42,6 +42,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">jQuery <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="selectElement.php">Sélection d'éléments</a></li>
+                        <li><a href="changeContent.php">Modifier le contenu d'un élément</a></li>
                     </ul>
                 </li>
             </ul>
